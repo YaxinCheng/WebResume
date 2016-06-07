8a702e828e40fcd8b9676b7455dc7789803dfbbe
WatashiJ <yx299612@dal.ca>
2016-06-07 12:01:48 -0300

    Recomplete the original version

---
+++ static/index.js
@@ -0,0 +1,8 @@
+!function() {
+    Array.prototype.forEach.call(document.querySelectorAll("[data-target]"), function(t) {
+        var e = t.getAttribute("data-target");
+        t.addEventListener("click", function() {
+            location.href = e
+        })
+    })
+}();
