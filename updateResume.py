ced5b2b3200356227c0454254c28f1c8cfbcc246
WatashiJ <yx299612@dal.ca>
2016-06-13 11:22:42 -0300

    Update Resume pdf file

---
+++ updateResume.py
@@ -0,0 +1,7 @@
+from shutil import copyfile
+import subprocess
+copyfile("""/Users/Cheng/Documents/Dal/Fall 2015/Co-op/Résumé.pdf""", """/Users/Cheng/Developer/Python/Resume/static/resume.pdf""")
+subprocess.call(["git", "add", "."])
+subprocess.call(["git", "commit", "-m", "Update Resume pdf file"])
+subprocess.call(["git", "push", "heroku", "master"])
+
