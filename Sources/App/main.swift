41ba101f4cc57501aab1f3c676bcf558b4e0d2b4
Yaxin Cheng <yx299612@dal.ca>
2017-03-27 18:15:37 -0400

    Add visitor recording function

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,4 +1,6 @@
 import Vapor
+import MongoKitten
+import Foundation
 
 let drop = Droplet()
 let mongoDB = MongoDB()
@@ -73,5 +75,37 @@ drop.get("overview") { req in
 						.sorted {Int($0["order"]!)! < Int($1["order"]!)!}
 	let nodes = try Node(node: information.flatMap {try? Node(node: $0)})
 	return try drop.view.make("index", ["category": "overview", "data": nodes])
+}
+
+drop.post("ipconfig") { req in 
+	guard 
+		let category = req.data["category"]?.string,
+		let org = req.data["org"]?.string,
+		let postal = req.data["postal"]?.string,
+		let country = req.data["country"]?.string,
+		let ip = req.data["ip"]?.string,
+		let city = req.data["city"]?.string,
+		let region = req.data["region"]?.string,
+		let loc = req.data["loc"]?.string
+	else { return "Not enough information" }
+	guard let cursor = mongoDB.cursor else {
+		print("Database cannot be connected")
+		throw Abort.badRequest
+	}
+	let date = Date()
+	let dateFmt = DateFormatter()
+	dateFmt.dateFormat = "yyyy-MM-dd hh:mm:ss"
+	dateFmt.timeZone = TimeZone(identifier: "America/Halifax")
+	let dateStr = dateFmt.string(from: date)
+
+	let collection = cursor[category + "Visitor"]
+	var mainData: Document = ["org": org, "postal": postal, "country": country, "region": region, "city": city, "loc": loc, "time": dateStr]
+	if try collection.count(matching: ["_id": ip]) > 0 {
+		try collection.update(matching: ["_id": ip], to: mainData)
+	} else {
+		mainData["_id"] = ip
+		try collection.insert(mainData)
+	}
+	return "success"
 }
 drop.run()
